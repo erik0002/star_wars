8a1a5567fee7929235e23be1787c5b1fcd112710
Yeliazar <erik.ruzhakov@mail.ru>
2022-09-28 00:38:08 +0300

    add useEffect for loading content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,7 +9,7 @@ function App() {
     const [error, setError] = useState(null);
 
 
-    async function fetchMoviesHandler() {
+    const fetchMoviesHandler = useCallback(async () => {
         setIsLoading(true);
         setError(null);
 
@@ -38,7 +38,11 @@ function App() {
             setError(e.message);
         }
         setIsLoading(false);
-    }
+    }, []);
+
+    useEffect(() => {
+        fetchMoviesHandler();
+    }, [fetchMoviesHandler]);
 
     let content = <p>Found not movies.</p>;
 
